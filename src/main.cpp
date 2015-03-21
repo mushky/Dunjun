fd2b08bc80879f52a8faafb4d344c2c3ff3fc90a
Ali Zaidi <msaZaidi@gmail.com>
2015-03-20 23:24:33 -0700

    OpenGL Window (GLFW3)
    Added Window in OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,67 @@
 #include <Dunjun/Common.hpp>
 
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+
+GLOBAL const int g_windowWidth = 854;
+GLOBAL const int g_windowHeight = 480;
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello!\n";
-	std::cin.ignore();
+	GLFWwindow* window;
 
-	return 0;
+	if (!glfwInit())
+		return EXIT_FAILURE;
+
+	window = glfwCreateWindow(g_windowWidth, g_windowHeight, "Dunjun", nullptr, nullptr);
+	if (!window)
+	{
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	bool running = true;
+	bool fullscreen = false;
+	while (running)
+	{
+		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		
+		if (glfwWindowShouldClose(window))
+			running = false;
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+			running = false;
+
+		if (glfwGetKey(window, GLFW_KEY_F11))
+		{
+			fullscreen = !fullscreen;
+
+			if (fullscreen)
+			{
+				GLFWwindow* newWindow;
+				newWindow = glfwCreateWindow(1920, 1080, "Dunjun", glfwGetPrimaryMonitor(), window);
+				glfwDestroyWindow(window);
+				window = newWindow;
+				glfwMakeContextCurrent(window);
+			}
+			else
+			{
+				GLFWwindow* newWindow;
+				newWindow = glfwCreateWindow(g_windowWidth, g_windowHeight, "Dunjun", nullptr, window);
+				glfwDestroyWindow(window);
+				window = newWindow;
+				glfwMakeContextCurrent(window);
+			}
+		}
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate(); 
+	return EXIT_SUCCESS;
 }
